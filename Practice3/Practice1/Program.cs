b318284cca15d5c9e0dcc0dc16862b1059a106b1
Ahmet Demir <admr61@gmail.com>
2018-10-02 22:59:46 +0200

    Updated Program.cs

--- Practice3/Practice1/Program.cs
+++ Practice3/Practice1/Program.cs
@@ -10,55 +10,111 @@ namespace Practice1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             
-            // InsertResults();
+            InsertResults();
             // GetAllResults();
             // GetMovies();
             // GetActors();
-            CountMovieActors();
+            // CountMovieActors();
 
         }
 
         static void InsertResults() {
             // Inserting entries into our database from here on, creating an instance of our context (MovieContext) with the keyword new (C# syntax & semantics.)
-            using (var _context = new MovieContext())
+               using (var _context = new MovieContext())
             {
                 // Inserting a new entry of Movie
-                // Movie m = new Movie {
-                //     Title = "Fight Club",
-                //     ReleaseYear = "1999",
-                //     Actors = new List<Actor> {
-                //         new Actor {
-                //             Name = "Brad Pitt",
-                //             Gender = "Male",
-                //             Birthdate = "25-05-1990"
-                //         },
-                //         new Actor {
-                //             Name = "Leonardo di Caprio",
-                //             Gender = "Female",
-                //             Birthdate = "20-09-2018"
-                //         }
-                //     }
-                // };
+                Movie m = new Movie
+                {
+                    Title = "Fight Club",
+                    ReleaseYear = "1999",
+                    Actors = new List<Actor> {
+                        new Actor {
+                            Name = "Brad Pitt",
+                            Gender = "Male",
+                            Birthdate = "25-05-1990"
+                        },
+                        new Actor {
+                            Name = "Leonardo di Caprio",
+                            Gender = "Female",
+                            Birthdate = "20-09-2018"
+                        },
+                        new Actor {
+                            Name = "Tom Hardy",
+                            Gender = "Male",
+                            Birthdate = "Male"
+                        }
+                    }
+                };
+                Movie m1 = new Movie
+                {
+                    Title = "Forest Gump",
+                    ReleaseYear = "2002",
+                    Actors = new List<Actor> {
+                        new Actor {
+                            Name = "Tom Hanks",
+                            Gender = "Male",
+                            Birthdate = "12-01-1955"
+                        },
+                        new Actor {
+                            Name = "Lebron James",
+                            Gender = "Female",
+                            Birthdate = "05-05-2030"
+                        }
+                    }
+                };
+                Movie m2 = new Movie {
+                    Title = "Fast and the Furious",
+                    ReleaseYear = "1997",
+                    Actors = new List<Actor> {
+                        new Actor {
+                            Name = "Paul Walker",
+                            Gender = "Male",
+                            Birthdate = "25-05-1980"
+                        },
+                        new Actor {
+                            Name = "Vin Diesel",
+                            Gender = "Male",
+                            Birthdate = "02-03-1972"
+                        },
+                        new Actor { 
+                            Name = "Ahmet Demir",
+                            Gender = "Male",
+                            Birthdate = "25-05-1997"
+                        },
+                        new Actor {
+                            Name = "Vincent Puyat",
+                            Gender = "Male",
+                            Birthdate = "07-10-1994"
+                        },
+                        new Actor {
+                            Name = "Rahit Tori",
+                            Gender = "Male",
+                            Birthdate = "25-10-1995"
+                        },
+                        new Actor {
+                            Name = "Anthony Elikwu",
+                            Gender = "Male",
+                            Birthdate = "06-03-1996"
+                        }
+                    }
+                };
 
                 // _context.Movies.Add(m); // add the results from the m variable to the database.
-                _context.SaveChanges(); // save the changes.
+                // _context.Movies.Add(m1);
+                // _context.Movies.Add(m2);
+                // _context.SaveChanges(); // save the changes.
 
                 // For-loop to read what we have currently in our database.
                 foreach (var movie in _context.Movies)
                 {
-                    Console.WriteLine("Movie: " + movie.Title + ", Release: " + movie.ReleaseYear);
-                    foreach (var actor in _context.Actors)
-                    {
-                        Console.WriteLine("Actor: " + actor.Name + ", Gender: " + actor.Gender + ", Birthdate: " + actor.Birthdate);
-                    }
+                    Console.WriteLine("Movie: {0}, Release: {1}", movie.Title, movie.ReleaseYear);
                 }
             }
         }
 
         // PROJECTION
-        static void GetMovies() { // For this method, we want to only show the movies, so we need to use a Projection here (select keyword.)
+        static void GetMovies() { // For this method, we want to show only the movies, so we need to use a Projection here (select keyword.)
             using (var _context = new MovieContext()) {
                 var results = from m in _context.Movies
                             select m;
@@ -70,7 +126,7 @@ namespace Practice1
             }   
         }
 
-        static void GetActors() { // For this method, we want to only show the actors, so we need to use a Projection here (select keyword.)
+        static void GetActors() { // For this method, we want to show only the actors, so we need to use a Projection here (select keyword.)
             using (var _context = new MovieContext()) {
                 var results = from a in _context.Actors
                             select a;
